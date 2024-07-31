f1144a6be4ded7c70e75f76a960156ff00c3ae79
Jiajun Mao <aaronmao.hangzhou@gmail.com>
2024-07-31 04:39:20 +0000

    dnode attribute to string

--- jni-lib/src/main/java/jni/DnodeAttributes.java
+++ jni-lib/src/main/java/jni/DnodeAttributes.java
@@ -14,5 +14,20 @@ public class DnodeAttributes {
     public int dcols;
     public int nparity;
     public List<Integer> childStatus;
-    
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("objset: ").append(objset).append("\n");
+        sb.append("object: ").append(object).append("\n");
+        sb.append("type: ").append(type).append("\n");
+        sb.append("path: ").append(path).append("\n");
+        sb.append("fsize: ").append(fsize).append("\n");
+        sb.append("numStripes: ").append(numStripes).append("\n");
+        sb.append("numRemainderStripes: ").append(numRemainderStripes).append("\n");
+        sb.append("dcols: ").append(dcols).append("\n");
+        sb.append("nparity: ").append(nparity).append("\n");
+        sb.append("childStatus: ").append(childStatus).append("\n");
+        return sb.toString();
+    }
 }
